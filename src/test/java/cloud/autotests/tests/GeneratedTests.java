93425f20e37415f6f47c40dbaf292aa0eceb19e2
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-04-02 12:06:52 +0300

    1.0 + chatbot

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -93,6 +94,94 @@ public class GeneratedTests extends TestBase {
         step("Провкручиваем страницу вверх через специальную кнопку", () -> {
             $("[class=BtnScroll_box__3XQTk]", 1).click();
         });
+    }
+
+    @Test
+    @Description("Checking change Titles Ru version")
+    @DisplayName("Beresnev")
+    void titleRUTestBeresnevVoc() {
+        step("Переходим на вкладку с вакансиями (RU) 'https://beresnev.games/ru'", () -> {
+            open("/ru/careers");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка Вакансии", () -> {
+            $("[class=text--h1]").shouldHave(text("Вакансии"));
+        });
+    }
+
+    @Test
+    @Description("Checking change Titles Ru version")
+    @DisplayName("Beresnev")
+    void titleRUTestBeresnevAbo() {
+        step("Переходим на вкладку с вакансиями (RU) 'https://beresnev.games/ru'", () -> {
+            open("/ru/about");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка О нас", () -> {
+            $("[class=text--h1]").shouldHave(text("О нас"));
+        });
+    }
+
+    @Test
+    @Description("Checking change Titles CZ version")
+    @DisplayName("Beresnev")
+    void titleCZTestBeresnevVoc() {
+        step("Переходим на вкладку с вакансиями (CZ) 'https://beresnev.games/cz'", () -> {
+            open("/cz/careers");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка Вакансии", () -> {
+            $("[class=text--h1]").shouldHave(text("Volná místa"));
+        });
+    }
+
+    @Test
+    @Description("Checking change Titles Cz version")
+    @DisplayName("Beresnev")
+    void titleCZTestBeresnevAbo() {
+        step("Переходим на вкладку с вакансиями (CZ) 'https://beresnev.games/cz'", () -> {
+            open("/cz/about");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка О нас", () -> {
+            $("[class=text--h1]").shouldHave(text("O nás"));
+        });
+
+
+    }
+
+    @Test
+    @Description("Checking change Titles EN version")
+    @DisplayName("Beresnev")
+    void titleENTestBeresnevVoc() {
+        step("Переходим на вкладку с вакансиями (EN) 'https://beresnev.games/en'", () -> {
+            open("/en/careers");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка Вакансии", () -> {
+            $("[class=text--h1]").shouldHave(text("Careers"));
+        });
+    }
+
+    @Test
+    @Description("Checking change Titles Cz version")
+    @DisplayName("Beresnev")
+    void titleENTestBeresnevAbo() {
+        step("Переходим на вкладку с вакансиями (EN) 'https://beresnev.games/en'", () -> {
+            open("/en/about");
+            sleep(200);
+        });
+
+        step("Проверяем Проверяем правильность написания заголовка О нас", () -> {
+            $("[class=text--h1]").shouldHave(text("About us"));
+        });
+
+
     }
 
 
